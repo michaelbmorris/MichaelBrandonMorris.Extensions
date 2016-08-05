2ec3fa643af90fd8be7c2e60e794060dedc5ff2e
Michael Brandon Morris <brandon@michaelbmorris.com>
2016-08-05 12:06:42 -0400

    Removed comma from append line csv. Edited doc comments.

--- MichaelBrandonMorris.Extensions/OtherExtensions/StringBuilderExtensions.cs
+++ MichaelBrandonMorris.Extensions/OtherExtensions/StringBuilderExtensions.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 using MichaelBrandonMorris.Extensions.PrimitiveExtensions;
 
 namespace MichaelBrandonMorris.Extensions.OtherExtensions
 {
     /// <summary>
-    /// Provides useful extensions for the <see cref="StringBuilder"/> class.
+    ///     Provides useful extensions for the <see cref="StringBuilder" /> 
+    ///     class.
     /// </summary>
     public static class StringBuilderExtensions
     {
@@ -16,8 +13,9 @@ namespace MichaelBrandonMorris.Extensions.OtherExtensions
         private const char Quote = '"';
 
         /// <summary>
-        /// Appends a string to the <see cref="StringBuilder"/>, wrapping it 
-        /// in quotes and adding a comma to the end for use in a .csv file.
+        ///     Appends a string to the <see cref="StringBuilder" />, wrapping 
+        ///     it in quotes and adding a comma to the end for use in a .csv 
+        ///     file.
         /// </summary>
         /// <param name="stringBuilder">The string builder.</param>
         /// <param name="s">The string to append.</param>
@@ -27,10 +25,17 @@ namespace MichaelBrandonMorris.Extensions.OtherExtensions
             stringBuilder.Append(s.Wrap(Quote) + Comma);
         }
 
+        /// <summary>
+        ///     Appends a string to the <see cref="StringBuilder" />, wrapping 
+        ///     it in quotes and adding a newline to the end for use in a .csv 
+        ///     file.
+        /// </summary>
+        /// <param name="stringBuilder">The string builder.</param>
+        /// <param name="s">The string to append.</param>
         public static void AppendLineCsv(
             this StringBuilder stringBuilder, string s)
         {
-            stringBuilder.AppendLine(s.Wrap(Quote) + Comma);
+            stringBuilder.AppendLine(s.Wrap(Quote));
         }
     }
 }
