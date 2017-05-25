251b5182b397b4cc7240f946af2200b780994874
Michael Brandon Morris <brandon@michaelbmorris.com>
2017-05-25 08:29:44 -0400

    Added HtmlHelper extensions. Added a new List extension.

--- MichaelBrandonMorris.Extensions/CollectionExtensions/ListExtensions.cs
+++ MichaelBrandonMorris.Extensions/CollectionExtensions/ListExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace MichaelBrandonMorris.Extensions.CollectionExtensions
@@ -35,6 +36,28 @@ namespace MichaelBrandonMorris.Extensions.CollectionExtensions
         {
             return list.Any() ?
                 list.Last() : default(T);
+        }
+
+        /// <summary>
+        /// Returns the shuffled <see cref="IList{T}"/>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public static IList<T> Shuffle<T>(this IList<T> list)
+        {
+            var random = new Random();
+            var n = list.Count;
+
+            for (var i = 0; i < n - 2; i++)
+            {
+                var j = random.Next(i, n);
+                var temp = list[i];
+                list[i] = list[j];
+                list[j] = temp;
+            }
+
+            return list;
         }
     }
 }
---
+++ MichaelBrandonMorris.Extensions/OtherExtensions/HtmlHelperExtensions.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace MichaelBrandonMorris.Extensions.OtherExtensions
+{
+    /// <summary>
+    /// Provides useful extensions for the <see cref="HtmlHelper{T}"/> class.
+    /// </summary>
+    public static class HtmlHelperExtensions
+    {
+        /// <summary>
+        /// Allows using <see cref="DisplayFor{TModel,TValue}"/> without an 
+        /// expression.
+        /// </summary>
+        /// <typeparam name="TModel"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="html"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static MvcHtmlString DisplayFor<TModel, TValue>(
+            this HtmlHelper<TModel> html,
+            TValue value)
+        {
+            return html.DisplayFor(x => value);
+        }
+
+        /// <summary>
+        /// Created by @mattlunn on GitHub at 
+        /// https://github.com/mattlunn/DynamicListBinding. Creates HTML 
+        /// editors for a list of properties.
+        /// </summary>
+        /// <typeparam name="TModel"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="html"></param>
+        /// <param name="propertyExpression"></param>
+        /// <param name="indexResolverExpression"></param>
+        /// <param name="includeIndexField"></param>
+        /// <returns></returns>
+        public static MvcHtmlString EditorForMany<TModel, TValue>(
+            this HtmlHelper<TModel> html,
+            Expression<Func<TModel, IEnumerable<TValue>>> propertyExpression,
+            Expression<Func<TValue, string>> indexResolverExpression = null,
+            bool includeIndexField = true) where TModel : class
+        {
+            var items = propertyExpression.Compile()(html.ViewData.Model);
+            var htmlBuilder = new StringBuilder();
+
+            var htmlFieldName =
+                ExpressionHelper.GetExpressionText(propertyExpression);
+
+            var htmlFieldNameWithPrefix =
+                html.ViewData.TemplateInfo.GetFullHtmlFieldName(htmlFieldName);
+
+            var indexResolver = indexResolverExpression?.Compile()
+                                ?? (x => null);
+
+            foreach (var item in items)
+            {
+                var dummy = new
+                {
+                    Item = item
+                };
+
+                var member = dummy.GetType().GetProperty("Item");
+
+                if (member == null)
+                {
+                    throw new Exception();
+                }
+
+                var memberExp = Expression.MakeMemberAccess(
+                    Expression.Constant(dummy),
+                    member);
+
+                var singleItemExp = Expression.Lambda<Func<TModel, TValue>>(
+                    memberExp,
+                    propertyExpression.Parameters);
+
+                var guid = indexResolver(item);
+
+                guid = string.IsNullOrEmpty(guid)
+                    ? Guid.NewGuid().ToString()
+                    : html.AttributeEncode(guid);
+
+                if (includeIndexField)
+                {
+                    htmlBuilder.Append(
+                        _EditorForManyIndexField(
+                            htmlFieldNameWithPrefix,
+                            guid,
+                            indexResolverExpression));
+                }
+
+                htmlBuilder.Append(
+                    html.EditorFor(
+                        singleItemExp,
+                        null,
+                        $"{htmlFieldName}[{guid}]"));
+            }
+
+            return new MvcHtmlString(htmlBuilder.ToString());
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TModel"></typeparam>
+        /// <param name="html"></param>
+        /// <param name="indexResolverExpression"></param>
+        /// <returns></returns>
+        public static MvcHtmlString EditorForManyIndexField<TModel>(
+            this HtmlHelper<TModel> html,
+            Expression<Func<TModel, string>> indexResolverExpression = null)
+        {
+            var htmlPrefix = html.ViewData.TemplateInfo.HtmlFieldPrefix;
+            var first = htmlPrefix.LastIndexOf('[');
+            var last = htmlPrefix.IndexOf(']', first + 1);
+
+            if (first == -1 || last == -1)
+            {
+                throw new InvalidOperationException(
+                    "EditorForManyIndexField called when not in a EditorForMany context");
+            }
+
+            var htmlFieldNameWithPrefix = htmlPrefix.Substring(0, first);
+            var guid = htmlPrefix.Substring(first + 1, last - first - 1);
+
+            return _EditorForManyIndexField(
+                htmlFieldNameWithPrefix,
+                guid,
+                indexResolverExpression);
+        }
+
+        private static MvcHtmlString _EditorForManyIndexField<TModel>(
+            string htmlFieldNameWithPrefix,
+            string guid,
+            Expression<Func<TModel, string>> indexResolverExpression)
+        {
+            var htmlBuilder = new StringBuilder();
+            htmlBuilder.AppendFormat(
+                @"<input type=""hidden"" name=""{0}.Index"" value=""{1}"" />",
+                htmlFieldNameWithPrefix,
+                guid);
+
+            if (indexResolverExpression != null)
+            {
+                htmlBuilder.AppendFormat(
+                    @"<input type=""hidden"" name=""{0}[{1}].{2}"" value=""{1}"" />",
+                    htmlFieldNameWithPrefix,
+                    guid,
+                    ExpressionHelper
+                        .GetExpressionText(indexResolverExpression));
+            }
+
+            return new MvcHtmlString(htmlBuilder.ToString());
+        }
+    }
+}
