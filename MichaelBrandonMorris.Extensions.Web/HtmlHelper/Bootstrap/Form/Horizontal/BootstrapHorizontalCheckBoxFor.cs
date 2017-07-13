4844be9960013022e62be895531592657e271f45
Michael Brandon Morris <brandon@michaelbmorris.com>
2017-07-13 15:28:37 -0400

    Add Bootstrap CheckBoxFor.

---
+++ MichaelBrandonMorris.Extensions.Web/HtmlHelper/Bootstrap/Form/Horizontal/BootstrapHorizontalCheckBoxFor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace MichaelBrandonMorris.Extensions.Web.HtmlHelper.Bootstrap.Form.Horizontal
+{
+    public static partial class InputExtensions
+    {
+        public static MvcHtmlString BootstrapHorizontalCheckBoxFor<TModel>(
+            this HtmlHelper<TModel> html,
+            Expression<Func<TModel, bool>> expression)
+        {
+            var checkBox = html.CheckBoxFor(expression);
+            var label = html.LabelFor(expression);
+
+            return MvcHtmlString.Create(
+                "<div class=\"form-group\">"
+                + "<div class=\"col-md-offset-2 col-md-10\">"
+                + "<div class=\"checkbox\">"
+                + checkBox
+                + label
+                + "</div>"
+                + "</div>"
+                + "/div");
+        }
+    }
+}
