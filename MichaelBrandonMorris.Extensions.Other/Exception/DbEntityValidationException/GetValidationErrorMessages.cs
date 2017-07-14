ba93296a68bc1d6a6c8acb1fc6c2b8cc442bb0a6
Michael Brandon Morris <brandon@michaelbmorris.com>
2017-07-14 09:18:53 -0400

    Added extension to get validation errors from DbEntityValidationException.

---
+++ MichaelBrandonMorris.Extensions.Other/Exception/DbEntityValidationException/GetValidationErrorMessages.cs
@@ -0,0 +1,25 @@
+﻿namespace MichaelBrandonMorris.Extensions.OtherExtensions.Exception.
+    DbEntityValidationException
+{
+    public static class DbEntityValidationExceptionExtensions
+    {
+        public static string GetValidationErrorMessages(
+            this System.Data.Entity.Validation.DbEntityValidationException
+                exception)
+        {
+            var stringBuilder = new System.Text.StringBuilder();
+
+            foreach (var entityValidationError in exception
+                .EntityValidationErrors)
+            {
+                foreach (var validationError in entityValidationError
+                    .ValidationErrors)
+                {
+                    stringBuilder.AppendLine(validationError.ErrorMessage);
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
